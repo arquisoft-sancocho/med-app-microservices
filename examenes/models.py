66922b4b985ee1e8afd3cbb9fdd0d3b5173482f7
AlejandraFruto <a.fruto@uniandes.edu.co>
2025-04-03 15:05:10 -0500

    changes model examenes

--- examenes/models.py
+++ examenes/models.py
@@ -1,21 +1,22 @@
 from django.db import models
 from pacientes.models import Paciente
 
-class TipoExamen(models.Model):
-    nombre = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.nombre
-
 class Examen(models.Model):
     RESULTADOS = [
         ('positivo', 'Positivo'),
         ('negativo', 'Negativo'),
     ]
+    
+    TIPOS = [
+        ('eeg', 'EEG'),
+        ( 'rm', 'Resonancia magnética'),
+        ( 'mirna', 'MicroRNA')
+        ( 'sangre', 'Muestra de sangre'),
+    ]
 
     paciente = models.ForeignKey(Paciente, on_delete=models.CASCADE)
     fecha_realizacion = models.DateField()
-    tipo_examen = models.ForeignKey(TipoExamen, on_delete=models.CASCADE)
+    tipo_examen = models.CharField(choices=TIPOS)
     resultado = models.CharField(max_length=10, choices=RESULTADOS)
 
     def __str__(self):
