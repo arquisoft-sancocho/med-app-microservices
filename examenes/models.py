25aa14284776d8852a1548252a9b924b6a13927f
AlejandraFruto <a.fruto@uniandes.edu.co>
2025-04-03 15:19:16 -0500

    fix length

--- examenes/models.py
+++ examenes/models.py
@@ -16,8 +16,8 @@ class Examen(models.Model):
 
     paciente = models.ForeignKey(Paciente, on_delete=models.CASCADE)
     fecha_realizacion = models.DateField()
-    tipo_examen = models.CharField(choices=TIPOS)
-    resultado = models.CharField(max_length=10, choices=RESULTADOS)
+    tipo_examen = models.CharField(max_length=10, choices=TIPOS)
+    resultado = models.CharField(max_length=35, choices=RESULTADOS)
 
     def __str__(self):
         return f"{self.tipo_examen} - {self.paciente}"
