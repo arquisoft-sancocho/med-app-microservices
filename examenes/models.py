d6520ae916d6753d5e162cc420373a19019e12ae
AlejandraFruto <a.fruto@uniandes.edu.co>
2025-04-03 19:04:27 -0500

    tipo_examen_id

--- examenes/models.py
+++ examenes/models.py
@@ -16,10 +16,10 @@ class Examen(models.Model):
 
     paciente = models.ForeignKey(Paciente, on_delete=models.CASCADE)
     fecha_realizacion = models.DateField()
-    tipo_examen = models.CharField(max_length=10, choices=TIPOS)
+    tipo_examen_id = models.CharField(max_length=10, choices=TIPOS)
     resultado = models.CharField(max_length=35, choices=RESULTADOS)
 
     def __str__(self):
-        return f"{self.tipo_examen} - {self.paciente}"
+        return f"{self.tipo_examen_id} - {self.paciente}"
 
 
