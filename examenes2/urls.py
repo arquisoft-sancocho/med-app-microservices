e45a67689a9e191eb03f564529b50944fcef004e
AlejandraFruto <a.fruto@uniandes.edu.co>
2025-05-01 15:31:01 -0500

    cambio url

--- examenes2/urls.py
+++ examenes2/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('examenes2/', views.examen_list2, name='examenList2'),
     path('examenes2/examenCreate2/', csrf_exempt(views.examen_create2), name='examenCreate2'),
     path('examen2/<int:examen_id>/', views.examen_detail2, name='examenDetail2'),
-    path('examen2/<int:examen_id>/delete/', views.examen_delete2, name='Delete2'),
+    path('examen2/<int:examen_id>/delete/', views.examen_delete2, name='examenDelete2'),
 ]
