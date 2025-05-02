baa51698859ce21d7ef3ee938a3c097ed5f2e935
AlejandraFruto <a.fruto@uniandes.edu.co>
2025-05-01 23:25:19 -0500

    cambios tacticas

--- core/urls.py
+++ core/urls.py
@@ -3,4 +3,10 @@ from . import views
 
 urlpatterns = [
     path('health/', views.health_check, name='health_check'),
+     # Ruta para la Sonda de Disponibilidad
+    path('health/ready', views.readiness_check, name='readiness_check'),
+    # Ruta para la Sonda de Actividad
+    path('health/live', views.liveness_check, name='liveness_check'),
+
+    
 ]
--- core/views.py
+++ core/views.py
@@ -1,8 +1,25 @@
-from django.http import JsonResponse
+import logging
+from django.http import HttpResponse, JsonResponse
+from django.db import connection, OperationalError
 
-def health_check(request):
-    """
-    Una vista simple que devuelve un estado 'ok'.
-    """
-    data = {"status": "ok"}
-    return JsonResponse(data)
+logger = logging.getLogger(_name_)
+
+def readiness_check(request):
+    db_ready = False
+    try:
+        with connection.cursor() as cursor:
+            cursor.execute("SELECT 1")
+        db_ready = True
+    except Exception as e:
+        logger.error(f"Readiness probe - DB connection error: {e}")
+        db_ready = False
+
+    is_ready = db_ready
+
+    if is_ready:
+        return HttpResponse("Ready", status=200, content_type="text/plain")
+    else:
+        return HttpResponse("Unavailable", status=503, content_type="text/plain")
+
+def liveness_check(request):
+    return HttpResponse("Alive", status=200, content_type="text/plain")
