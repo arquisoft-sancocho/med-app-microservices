4a01a0ee1b9e9fc1741c344e1d806e59b4ae5f42
AlejandraFruto <a.fruto@uniandes.edu.co>
2025-05-06 23:43:47 -0500

    fix base

--- medical_system/views.py
+++ medical_system/views.py
@@ -3,8 +3,5 @@ from django.contrib.auth.decorators import login_required
 
 @login_required
 def index(request):
-    grupos_restringidos = ['Tecnico', 'Enfermero']
-    tiene_permiso_extra = not request.user.groups.filter(name__in=grupos_restringidos).exists()
-    tiene_permiso_unico = not request.user.groups.filter(name__in= ["Tecnico"]).exists()
-    return render(request, 'index.html', {'tiene_permiso_extra': tiene_permiso_extra}, {'tiene_permiso_unico': tiene_permiso_unico})
+    return render(request, 'index.html')
 
